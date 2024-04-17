b1b988ea9021bb81f6f07a32aa033873918a4081
anas-makhool <anasmkhool@gmail.com>
2024-04-18 01:31:29 +0200

    UPD: mian.js

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const startBtn = document.querySelector(".start-btn button");
 const infoBox = document.querySelector(".info-box");
 const quizBox = document.querySelector(".quiz-box");
 const nextBtn = document.querySelector(".next-btn");
+const timerSec = document.querySelector(".timer-sec");
+const progress = document.querySelector(".progress");
+
 let currentIndex = 0;
 let score = 0;
+let startProg = 0;
+let interval;
 
-const showOrHide = function (check, ele) {
+const addRem_show = function (check, ele) {
   if (check) {
     ele.classList.add("show");
   } else {
@@ -13,14 +18,22 @@ const showOrHide = function (check, ele) {
   }
 };
 
-startBtn.onclick = () => showOrHide(true, infoBox);
+const addRem_event = function (check, ele) {
+  if (check) {
+    ele.classList.add("event-none");
+  } else {
+    ele.classList.remove("event-none");
+  }
+};
 
-infoBox.querySelector(".quit").onclick = () => showOrHide(false, infoBox);
+startBtn.onclick = () => addRem_show(true, infoBox);
 
-infoBox.querySelector(".restart").onclick = () => {
-  showOrHide(false, infoBox);
-  showOrHide(false, nextBtn);
-  showOrHide(true, quizBox);
+infoBox.querySelector(".quit").onclick = () => addRem_show(false, infoBox);
+
+const startQue = function () {
+  addRem_show(false, infoBox);
+  addRem_show(false, nextBtn);
+  addRem_show(true, quizBox);
 
   let currentQue = questions[currentIndex];
   let rightAnswer = currentQue.right_answer;
@@ -57,29 +70,33 @@ infoBox.querySelector(".restart").onclick = () => {
     wrongEffect.innerHTML = `<div class="icon cross"><i class="fas fa-times"></i></div>`;
 
     ele.onclick = () => {
-      const chosenAnswer = ele.querySelector(".option span").innerText;
+      addRem_event(true, ele.parentElement);
+      const chosenAnswer = ele.querySelector(".option span").innerHTML;
 
       if (chosenAnswer == rightAnswer) {
         ele.insertAdjacentElement("beforeend", rightEffect);
         score++;
       } else {
         quizBox.querySelectorAll(".option").forEach((e) => {
-          if (e.querySelector(".option span").innerText === rightAnswer) {
+          if (e.querySelector(".option span").innerHTML === rightAnswer) {
             e.insertAdjacentElement("beforeend", rightEffect);
           }
         });
         ele.insertAdjacentElement("beforeend", wrongEffect);
       }
 
-      showOrHide(true, nextBtn);
-      nextBtn.onclick = () => {
-        showOrHide(true, infoBox);
-      };
+      addRem_show(true, nextBtn);
     };
   });
 };
 
-// for (let i = 0; i < questions.length; i++) {
-//   const ele = questions[i];
-//   console.log(ele);
-// }
+infoBox.querySelector(".restart").onclick = () => startQue();
+
+nextBtn.onclick = () => {
+  if (currentIndex === questions.length - 1) return;
+  currentIndex++;
+  console.log("currentIndex: " + currentIndex);
+  console.log(`score: ` + score);
+  startQue();
+  addRem_show(false, nextBtn);
+};

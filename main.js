c31305c1294fd4ccc89205ccd0ae28ddc5c4831d
anas-makhool <anasmkhool@gmail.com>
2024-04-18 06:16:22 +0200

    UPD: main.js

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const quizBox = document.querySelector(".quiz-box");
 const resultBox = document.querySelector(".result-box");
 const nextBtn = document.querySelector(".next-btn");
 const resultText = document.querySelector(".score-text");
-const resultP1 = resultText.children[0].firstElementChild;
-const resultP2 = resultText.children[0].lastElementChild;
+const resultP1 = resultText.children[0].children[0];
+const resultP2 = resultText.children[0].children[1];
+const resultP3 = resultText.children[0].children[2];
 const timerSec = document.querySelector(".timer-sec");
 const progress = document.querySelector(".progress");
 const total = document.querySelector(".total-que");
 const totalP1 = total.children[0].firstElementChild;
 const totalP2 = total.children[0].lastElementChild;
+const replayQuiz = document.querySelector(".result-box .restart");
+const quitQuiz = document.querySelector(".result-box .quit");
 const parentFooter = document.querySelector(".parent");
-console.log(resultText);
 const rightEffect = document.createElement("div");
+const closeIcon = document.querySelector(".x");
 rightEffect.innerHTML = `<div class="icon tick"><i class="fas fa-check"></i></div>`;
 const wrongEffect = rightEffect.cloneNode(false);
 wrongEffect.innerHTML = `<div class="icon cross"><i class="fas fa-times"></i></div>`;
@@ -39,7 +42,11 @@ const addRem_event = function (check, ele) {
   }
 };
 
-startBtn.onclick = () => addRem_show(true, infoBox);
+startBtn.onclick = () => {
+  addRem_show(true, infoBox);
+  score = 0;
+  currentIndex = 0;
+};
 
 infoBox.querySelector(".quit").onclick = () => addRem_show(false, infoBox);
 
@@ -50,6 +57,7 @@ const startQue = function () {
 
   let currentQue = questions[currentIndex];
   let rightAnswer = currentQue.right_answer;
+  console.log(rightAnswer);
 
   let quizBlock = `
   <div class="que-text">
@@ -101,13 +109,29 @@ infoBox.querySelector(".restart").onclick = () => {
   startTimer(timeValue);
 };
 
+function checkResultTitle(score, questions) {
+  if (score === questions) {
+    return { text: "Perfect", color: "#007bff" };
+  } else if (score > questions / 2) {
+    return { text: "Good", color: "#23903c" };
+  } else if (score < questions / 2 && score > 0) {
+    return { text: "Sorry", color: "#a42834" };
+  } else if (score === 0) {
+    return { text: "Sorry 😥", color: "#a42834" };
+  }
+}
+
 nextBtn.onclick = function () {
   if (currentIndex === questions.length - 1) {
     console.log(`score: ` + score);
     addRem_show(false, quizBox);
     addRem_show(true, resultBox);
-    resultP1.innerHTML = score;
-    resultP2.innerHTML = questions.length;
+    let result = checkResultTitle(score, questions.length);
+    resultP1.innerHTML = result.text;
+    resultP1.style.color = result.color;
+
+    resultP2.innerHTML = score;
+    resultP3.innerHTML = questions.length;
     return;
   }
   currentIndex++;
@@ -121,7 +145,7 @@ nextBtn.onclick = function () {
 };
 
 function startTimer(time) {
-  counter = setInterval(timer, 10);
+  counter = setInterval(timer, 1000);
   function timer() {
     timerSec.textContent = time;
     time--;
@@ -139,3 +163,31 @@ function startTimer(time) {
     }
   }
 }
+
+replayQuiz.onclick = () => {
+  console.log(`replay`);
+  score = 0;
+  currentIndex = 0;
+  addRem_show(false, resultBox);
+  addRem_show(true, quizBox);
+  totalP1.innerHTML = 1;
+  let result = checkResultTitle(score, questions.length);
+  startQue();
+  resultP1.innerHTML = result.text;
+  resultP1.style.color = result.color;
+};
+
+quitQuiz.onclick = () => {
+  totalP1.innerHTML = 1;
+  addRem_show(false, resultBox);
+};
+
+closeIcon.onclick = () => {
+  console.log(`oww`);
+  totalP1.innerHTML = 1;
+  clearInterval(counter);
+  setTimeout(() => {
+    timerSec.innerHTML = timeValue;
+  }, 300);
+  addRem_show(false, quizBox);
+};
